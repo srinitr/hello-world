4ede445657224df960fe9f3cf5277bd3d93b01cb
srinitr <srinitr69@gmail.com>
2016-08-09 11:51:56 +0530

    added new user profile java file

---
+++ user_profile.java
@@ -0,0 +1,20 @@
+class userprofile {
+
+public static void main(String[] args) {
+
+System.out.println("Enter User First Name");
+System.out.println("Enter User Last Name");
+System.out.println("Enter User State");
+System.out.println("Enter User City");
+System.out.println("Select User DoB");
+System.out.println("Select User DoM");
+  {
+    #Error checking for date of birth
+	  
+  }
+Save.Button();
+Cancel.Button();
+on Save operation 
+  Update SQL and make a commit
+  }
+}
